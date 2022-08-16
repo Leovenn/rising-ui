28c00d9b5c60b21cff2f83fb9f943d0aa93d40c4
leovenn <leovenn@163.com>
2022-08-16 22:54:10 +0800

    🐎 ci(setup语法): 增强

--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -2,13 +2,8 @@
   <div>123</div>
 </template>
 
-<script setup lang="ts" name="666">
-import { defineComponent } from 'vue'
-</script>
-<script lang="ts">
-export default defineComponent({
-  name: 'RButton',
-})
+<script setup lang="ts">
+defineOptions({ name: 'RButton' })
 </script>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+/** 增强vue setup 语法 */
+import VueMacros from 'unplugin-vue-macros/vite'
+/** 新增导入类型 */
 import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
@@ -31,7 +33,7 @@ export default defineConfig((configEnv) => {
         outputDir: resolve(__dirname, 'sage-ui/lib'),
         tsConfigFilePath: './tsconfig.json',
       }),
-      // dts(),
+      VueMacros(),
     ],
 
     /**配置别名 */
