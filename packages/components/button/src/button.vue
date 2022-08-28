9479f36bc27a4d0916d728be2f1ced7d1c4dce5f
leovenn <leovenn@163.com>
2022-08-28 23:58:41 +0800

    ✨ feat(button): dev

--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -1,8 +1,17 @@
 <template>
-  <button class="el-button">123456</button>
+  <button class="r-button">default</button>
+  <button class="r-button r-button--primary r-button--small">default</button>
+  <button class="r-button r-button--success r-button--large">default</button>
+  <button class="r-button r-button--warning">default</button>
+  <button class="r-button r-button--error">default</button>
+  <button class="r-button r-button--info">default</button>
 </template>
 <script setup lang="ts">
 // defineOptions({ name: 'RButton' })
 </script>
 
-<style scoped></style>
+<style scoped>
+button {
+  margin-right: 10px;
+}
+</style>
